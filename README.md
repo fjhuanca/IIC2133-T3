98ebc7d9022166b858e221684ac36f3f776321fa
inzuniga <inzuniga@uc.cl>
2021-06-24 11:39:36 -0400

    add base files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tarea 3
+Código base para tu tarea.
+
+## Compilar y ejecutar
+Compilar:
+```make```
+
+Ejecutar:
+```./dewey <network.txt> <output.txt>```
---
+++ src/dewey/main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char** argv)
+{
+    // Revisamos los argumentos
+    if(argc != 3) {
+        printf("Modo de uso: %s <network.txt> <output.txt>\n", argv[0]);
+        return 1;
+    }
+
+    /* ------------- POR IMPLEMENTAR -------------- */
+    /* El código de tu tarea va aquí.               */
+
+
+    // Terminamos exitosamente
+    return 0;
+}
